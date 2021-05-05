e2929b454ae255a425795b7cda71533dfaa217da
vesper1978 <28059535+vesper1978@users.noreply.github.com>
2021-05-05 05:49:53 -0300

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
+# Prepare the system
+pw useradd -n qbt -u 8675309 -m -c "qbittorrent Client" -s /usr/sbin/nologin -w no
+
+mkdir -p /home/qbt/.config/qbt
+chown -R qbt:qbt /home/qbt/
+
+mkdir /Downloads
+chown qbt:qbt /Downloads
+
 #Install flavor port
 pkg install qbittorrent-nox
 
-#auto start after jail reboot
-echo /usr/local/bin/qbittorrent-nox -d > /etc/rc.local
+sysrc -f /etc/rc.conf qbittorrent_enable="YES"
+sysrc -f /etc/rc.conf qbittorrent_user="qbt"
 
-#fist start
-/usr/local/bin/qbittorrent-nox -d
+sed -i '' 's|/var/db/qbittorrent/conf|/home/qbt/.config/qbt|g' /usr/local/etc/rc.d/qbittorrent
+sed -i '' 's|/var/db/qbittorrent/Downloads|/Downloads|g' /usr/local/etc/rc.d/qbittorrent
+
+service qbittorrent start
 
 echo "Please open the URL and Login with Username: admin, Password: adminadmin" > /root/PLUGIN_INFO
